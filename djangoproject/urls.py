66e02bc89043fd85853f70ad2c18172c3d3a758e
Divya Nayal <43030745+divyanayal@users.noreply.github.com>
2021-04-07 01:16:37 +0530

    Made User Registration Form

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),    #include function chops off the part which is already processed, 
+    path('register/', user_views.register, name='register'),
+    path('', include('blog.urls')),    #include function chops off the part which is already processed, 
                                             #in this case blog is already processed hence empty string is sent to the blog.urls module.
     
 ]
